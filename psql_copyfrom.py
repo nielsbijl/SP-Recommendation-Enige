daa9154ff38a4c1e687e2e21c700858f59a5a329
nielsbijl <60733165+nielsbijl@users.noreply.github.com>
2020-03-20 20:48:36 +0100

    commit

---
+++ psql_copyfrom.py
@@ -0,0 +1,15 @@
+import psycopg2
+
+c = psycopg2.connect("dbname=postgres user=postgres password=niels16")  #TODO: edit this.
+cur = c.cursor()
+
+filenames = ['products', 'profiles', 'profiles_previously_viewed', 'sessions', 'equalproducts', 'segmentPopulairProduct']
+for filename in filenames:
+    with open(filename+'.csv') as csvfile:
+        print("Copying {}...".format(filename))
+        cur.copy_expert("COPY "+filename+" FROM STDIN DELIMITER ',' CSV HEADER", csvfile)
+        c.commit()
+
+c.commit()
+cur.close()
+c.close()
